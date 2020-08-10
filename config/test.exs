20789411bd24513c4798f9fa76a11193fd05deb9
Erik Hanson <erik@geometer.io>
2020-08-10 16:09:01 -0700

    try using "POSTGRES_HOST" env var to make gitlab ci happy

--- config/test.exs
+++ config/test.exs
@@ -6,11 +6,11 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :epicenter, Epicenter.Repo,
-  username: "postgres",
-  password: "postgres",
   database: "epicenter_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  password: "postgres",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  username: "postgres"
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
