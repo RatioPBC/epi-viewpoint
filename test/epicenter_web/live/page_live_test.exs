af55a61582c086e2605236e7bab5269b03975d2c
Erik Hanson <erik@geometer.io>
2020-08-10 17:25:36 -0700

    simple styling

--- test/epicenter_web/live/page_live_test.exs
+++ test/epicenter_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule EpicenterWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "No data here yet"
+    assert render(page_live) =~ "No data here yet"
   end
 end
