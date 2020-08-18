7a2338b0581d355edf886e38fb98062e0b9d0c93
Erik Hanson <erik@geometer.io>
2020-08-18 11:41:45 -0700

    csv allows tids

--- lib/epicenter/cases/import.ex
+++ lib/epicenter/cases/import.ex
@@ -3,10 +3,15 @@ defmodule Epicenter.Cases.Import do
   alias Epicenter.Csv
   alias Epicenter.DateParser
 
-  @lab_result_fields ~w{result result_date sample_date}
-  @person_fields ~w{dob first_name last_name}
+  @required_lab_result_fields ~w{result result_date sample_date}
+  @optional_lab_result_fields ~w{lab_result_tid}
+  @required_person_fields ~w{dob first_name last_name}
+  @optional_person_fields ~w{person_tid}
 
-  @fields [required: @lab_result_fields ++ @person_fields, optional: ~w{}]
+  @fields [
+    required: @required_lab_result_fields ++ @required_person_fields,
+    optional: @optional_lab_result_fields ++ @optional_person_fields
+  ]
 
   def from_csv(csv_string) do
     {:ok, rows} = Csv.read(csv_string, @fields)
@@ -16,15 +21,17 @@ defmodule Epicenter.Cases.Import do
         %{people: people, lab_results: lab_results} ->
           person =
             row
-            |> Map.take(@person_fields)
+            |> Map.take(@required_person_fields ++ @optional_person_fields)
             |> Map.update!("dob", &DateParser.parse_mm_dd_yyyy!/1)
+            |> Euclid.Extra.Map.rename_key("person_tid", "tid")
             |> Cases.upsert_person!()
 
           lab_result =
             row
-            |> Map.take(@lab_result_fields)
+            |> Map.take(@required_lab_result_fields ++ @optional_lab_result_fields)
             |> Map.update!("sample_date", &DateParser.parse_mm_dd_yyyy!/1)
             |> Map.put("person_id", person.id)
+            |> Euclid.Extra.Map.rename_key("lab_result_tid", "tid")
             |> Cases.create_lab_result!()
 
           %{people: [person.id | people], lab_results: [lab_result.id | lab_results]}
--- test/epicenter/cases/import_test.exs
+++ test/epicenter/cases/import_test.exs
@@ -7,19 +7,24 @@ defmodule Epicenter.Cases.ImportTest do
   describe "from_csv" do
     test "creates LabResult records and Person records from csv data" do
       """
-      first_name , last_name , dob        , thing, sample_date , result_date , result   , glorp
-      Alice      , Ant       , 01/02/1970 , graz , 06/01/2020  , 06/03/2020  , positive , 393
-      Billy      , Bat       , 03/04/1990 , fnord, 06/06/2020  , 06/07/2020  , negative , sn3
+      first_name , last_name , dob        , thing, sample_date , result_date , result   , glorp , person_tid , lab_result_tid
+      Alice      , Ant       , 01/02/1970 , graz , 06/01/2020  , 06/03/2020  , positive , 393   , alice      , alice-result-1
+      Billy      , Bat       , 03/04/1990 , fnord, 06/06/2020  , 06/07/2020  , negative , sn3   , billy      , billy-result-1
       """
       |> Import.from_csv()
       |> assert_eq({:ok, %{people: 2, lab_results: 2}})
 
       [lab_result_1, lab_result_2] = Cases.list_lab_results()
+      assert lab_result_1.tid == "alice-result-1"
       assert lab_result_1.result == "positive"
+
+      assert lab_result_2.tid == "billy-result-1"
       assert lab_result_2.result == "negative"
 
       [alice, billy] = Cases.list_people()
+      assert alice.tid == "alice"
       assert alice.first_name == "Alice"
+      assert billy.tid == "billy"
       assert billy.first_name == "Billy"
     end
 
