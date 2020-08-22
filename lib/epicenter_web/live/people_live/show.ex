92ac347c52026f6d98b66b450d05d231d1df513e
Erik Hanson <erik@geometer.io>
2020-08-21 18:24:23 -0700

    more information on person/show

--- lib/epicenter_web/live/people_live/show.ex
+++ lib/epicenter_web/live/people_live/show.ex
@@ -2,9 +2,14 @@ defmodule EpicenterWeb.PeopleLive.Show do
   use EpicenterWeb, :live_view
 
   alias Epicenter.Cases
+  alias Epicenter.Cases.Person
 
   def mount(%{"id" => id}, _session, socket) do
     person = Cases.get_person(id) |> Cases.preload_lab_results()
     {:ok, assign(socket, person: person)}
+  end
+
+  def age(%Person{dob: dob}) do
+    Date.diff(Date.utc_today(), dob) |> Integer.floor_div(365)
   end
 end
