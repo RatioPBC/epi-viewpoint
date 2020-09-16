6dcbb503ea6077dbc641363d878496da13f04510
Erik Hanson <erik@geometer.io>
2020-09-15 17:15:52 -0700

    add more stuff to styleguide
    Signed-off-by: Pat Maddox <pat@geometer.io>

--- lib/epicenter_web/live/styleguide_live.ex
+++ lib/epicenter_web/live/styleguide_live.ex
@@ -1,6 +1,8 @@
 defmodule EpicenterWeb.StyleguideLive do
   use EpicenterWeb, :live_view
 
+  import EpicenterWeb.IconView
+
   def mount(_params, _session, socket) do
     {:ok, assign(socket, show_nav: false)}
   end
