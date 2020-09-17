0776254e795a8046d530143dbf0d24958cd7a337
Sara Tansey <sara@geometer.io>
2020-09-17 15:30:12 -0700

    add date of birth to profile edit page
    Signed-off-by: Erik Hanson <erik@geometer.io>

--- test/epicenter_web/live/profile_edit_live_test.exs
+++ test/epicenter_web/live/profile_edit_live_test.exs
@@ -17,16 +17,20 @@ defmodule EpicenterWeb.ProfileEditLiveTest do
     assert_has_role(page_live, "person-edit-page")
   end
 
-  test "editing person identifying information", %{conn: conn} do
+  test "editing person identifying information works, saves an audit trail, and redirects to the profile page", %{conn: conn} do
     user = Test.Fixtures.user_attrs("user") |> Accounts.create_user!()
     person = Test.Fixtures.person_attrs(user, "alice") |> Cases.create_person!()
 
     {:ok, page_live, _html} = live(conn, "/people/#{person.id}/edit")
 
-    params = %{"person" => %{"first_name" => "Aaron"}}
-    {:ok, redirected_view, _} = page_live |> render_submit("save", params) |> follow_redirect(conn)
+    {:ok, redirected_view, _} =
+      page_live
+      |> form("#person-form", person: %{first_name: "Aaron", last_name: "Testuser2", dob: "2020-01-01"})
+      |> render_submit()
+      |> follow_redirect(conn)
 
-    assert_role_text(redirected_view, "full-name", "Aaron Testuser")
+    assert_role_text(redirected_view, "full-name", "Aaron Testuser2")
+    assert_role_text(redirected_view, "date-of-birth", "01/01/2020")
     assert_versioned(person, expected_count: 2)
   end
 end
