2c8bb51b876b7361bdec5fccf4e6df3f2bbaf44b
Erik Hanson <erik@geometer.io>
2020-09-24 18:23:27 -0700

    sometimes a parsed doc is a tuple, not a list, so check the specific thing that was being guarded against, which is passing a string into the Html function

--- test/support/html.ex
+++ test/support/html.ex
@@ -1,14 +1,14 @@
 defmodule Epicenter.Test.Html do
-  def all(html, css_query, as: :text) when is_list(html),
+  def all(html, css_query, as: :text) when not is_binary(html),
     do: html |> all(css_query, &Floki.text/1)
 
-  def all(html, css_query, as: :tids) when is_list(html),
+  def all(html, css_query, as: :tids) when not is_binary(html),
     do: html |> all(css_query, &tid/1) |> List.flatten()
 
-  def all(html, css_query, attr: attr) when is_list(html),
+  def all(html, css_query, attr: attr) when not is_binary(html),
     do: html |> all(css_query, &Floki.attribute(&1, attr)) |> List.flatten()
 
-  def all(html, css_query, fun) when is_list(html) and is_function(fun),
+  def all(html, css_query, fun) when not is_binary(html) and is_function(fun),
     do: html |> Floki.find(css_query) |> Enum.map(fun)
 
   def attr(html, css_query \\ "*", attr_name) when not is_binary(html),
@@ -17,10 +17,10 @@ defmodule Epicenter.Test.Html do
   def has_role?(html, role),
     do: html |> Floki.find("[data-role=#{role}]") |> Euclid.Exists.present?()
 
-  def html(html, css_query) when is_list(html),
+  def html(html, css_query) when not is_binary(html),
     do: html |> Floki.find(css_query) |> Enum.map(&Floki.raw_html/1)
 
-  def meta_contents(html, name) when is_list(html),
+  def meta_contents(html, name) when not is_binary(html),
     do: html |> Floki.attribute("meta[name=#{name}]", "content") |> Enum.join("")
 
   def parse(html_string),
@@ -29,7 +29,7 @@ defmodule Epicenter.Test.Html do
   def parse_doc(html_string),
     do: html_string |> Floki.parse_document!()
 
-  def page_title(html) when is_list(html),
+  def page_title(html) when not is_binary(html),
     do: html |> html("title") |> Euclid.Extra.Enum.first!() |> parse() |> Floki.text()
 
   def role_text(html, role),
@@ -38,10 +38,10 @@ defmodule Epicenter.Test.Html do
   def role_texts(html, role),
     do: html |> all("[data-role=#{role}]", as: :text)
 
-  def text(html) when is_list(html) or is_tuple(html),
+  def text(html) when not is_binary(html) or is_tuple(html),
     do: html |> Floki.text(sep: " ")
 
-  def text(html, css_query) when is_list(html),
+  def text(html, css_query) when not is_binary(html),
     do: html |> Floki.find(css_query) |> Floki.text()
 
   defp tid(html),
