f7137da2ffbc3b3973557f050038526a34ff9af0
Erik Hanson <erik@geometer.io>
2020-10-02 16:00:55 -0700

    add release command to reset a user's password

--- lib/epicenter/release.ex
+++ lib/epicenter/release.ex
@@ -6,29 +6,28 @@ defmodule Epicenter.Release do
   def create_user(username, email) do
     ensure_started()
 
-    IO.puts("Creating user #{username} / #{email}...")
+    IO.puts("Creating user #{username} / #{email}; they must set their password via this URL:")
 
     case Accounts.register_user(%{email: email, password: Euclid.Extra.Random.string(), username: username}) do
       {:ok, user} ->
-        Accounts.deliver_user_reset_password_instructions(user, fn encoded_token ->
-          url = Routes.user_reset_password_url(Endpoint, :edit, encoded_token)
-
-          """
-
-          Success! They must set their password via this URL:
-
-          #{url}
-
-          """
-          |> IO.puts()
-        end)
-
+        generated_password_reset_url(user)
         :ok
 
       {:error, %Ecto.Changeset{errors: errors}} ->
         IO.puts("FAILED!")
         {:error, errors}
     end
+  end
+
+  def reset_password(email) do
+    Accounts.get_user_by_email(email) |> generated_password_reset_url()
+    :ok
+  end
+
+  defp generated_password_reset_url(user) do
+    Accounts.deliver_user_reset_password_instructions(user, fn encoded_token ->
+      Routes.user_reset_password_url(Endpoint, :edit, encoded_token) |> IO.puts()
+    end)
   end
 
   defp ensure_started do
