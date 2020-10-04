959ffb665aabc03f2f68c5f137cca6f8c7ec058f
Erik Hanson <erik@geometer.io>
2020-10-04 14:03:03 -0700

    fix compiler warning

--- test/support/pages/mfa_settings.ex
+++ test/support/pages/mfa_settings.ex
@@ -1,6 +1,5 @@
 defmodule EpicenterWeb.Test.Pages.MfaSettings do
   alias EpicenterWeb.Test.Pages
-  alias Plug.Conn
 
   def assert_here(view_or_conn_or_html),
     do: view_or_conn_or_html |> Pages.assert_on_page("mfa-settings")
