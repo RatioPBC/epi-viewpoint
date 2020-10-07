464ed3215db5a4769042b2357f169ac733758d5b
Erik Hanson <erik@geometer.io>
2020-10-07 12:24:55 -0700

    styling cleanup
    Signed-off-by: Harold Tafur <harold@geometer.io>

--- lib/epicenter_web/views/user_reset_password_view.ex
+++ lib/epicenter_web/views/user_reset_password_view.ex
@@ -1,5 +1,5 @@
 defmodule EpicenterWeb.UserResetPasswordView do
   use EpicenterWeb, :view
 
-  import EpicenterWeb.IconView, only: [error_icon: 0, logo_icon: 2]
+  import EpicenterWeb.IconView, only: [error_icon: 0, login_icon: 0]
 end
