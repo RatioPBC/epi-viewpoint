39dcfc11fa10dd6073b76ec5b4c22f111aaf7af1
Mike Stallard <mike@geometer.io>
2020-10-12 18:07:06 -0700

    feat: two digit years are handled correctly in parsing
    [#175237020]
    
    Signed-off-by: Erik Hanson <erik@geometer.io>

--- lib/epicenter/date_parser.ex
+++ lib/epicenter/date_parser.ex
@@ -1,5 +1,5 @@
 defmodule Epicenter.DateParser do
-  @mm_dd_yyyy_regex ~r|(?<mm>\d\d?)[-/](?<dd>\d\d?)[-/](?<yyyy>\d{4})|
+  @mm_dd_yyyy_regex ~r|(?<mm>\d\d?)[-/](?<dd>\d\d?)[-/](?<yyyy>\d{2,4})|
 
   def parse_mm_dd_yyyy(nil), do: {:ok, nil}
   def parse_mm_dd_yyyy(""), do: {:ok, nil}
@@ -10,6 +10,8 @@ defmodule Epicenter.DateParser do
     year = captures["yyyy"] |> to_integer()
     month = captures["mm"] |> to_integer()
     day = captures["dd"] |> to_integer()
+
+    year = normalize_year(year)
 
     if [{year, 1850..2050}, {month, 1..12}, {day, 1..31}] |> Enum.all?(&valid?/1),
       do: Date.new(year, month, day),
@@ -24,6 +26,18 @@ defmodule Epicenter.DateParser do
     case parse_mm_dd_yyyy(mm_dd_yyyy) do
       {:ok, date} -> date
       {:error, message} -> raise Epicenter.DateParsingError, message
+    end
+  end
+
+  defp normalize_year(year) do
+    current_year = Date.utc_today().year
+    current_century = current_year - rem(current_year, 100)
+    last_century = current_century - 100
+
+    cond do
+      year > 99 -> year
+      current_century + year <= current_year -> current_century + year
+      true -> last_century + year
     end
   end
 
