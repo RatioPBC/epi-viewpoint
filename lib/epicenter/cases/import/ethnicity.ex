e3c7e3045a8c5a9cfb75c2d63f8c58be7a4b3966
Mike Stallard <mike@geometer.io>
2020-10-14 14:38:43 -0700

    feat: map ODRS ethnicities to Epicenter
    [#174656418]
    
    Signed-off-by: Harold Tafur <harold@geometer.io>

--- lib/epicenter/cases/import/ethnicity.ex
+++ lib/epicenter/cases/import/ethnicity.ex
@@ -1,6 +1,14 @@
 defmodule Epicenter.Cases.Import.Ethnicity do
+  @ethnicity_mapping %{
+    "RefusedToAnswer" => %{"parent" => "Declined to answer", "children" => []},
+    "NonHispanicOrNonLatino" => %{"parent" => "Not Hispanic, Latino/a, or Spanish origin", "children" => []},
+    "HispanicOrLatino" => %{"parent" => "Hispanic, Latino/a, or Spanish origin", "children" => []}
+  }
+
+  @unknown_ethnicity %{"parent" => nil, "children" => nil}
+
   def build_attrs(%{"ethnicity" => ethnicity} = attrs),
-    do: attrs |> Map.put("ethnicity", %{"parent" => ethnicity, "children" => []})
+    do: attrs |> Map.put("ethnicity", @ethnicity_mapping[ethnicity] || @unknown_ethnicity)
 
   def build_attrs(attrs),
     do: attrs
--- test/epicenter/cases/import/ethnicity_test.exs
+++ test/epicenter/cases/import/ethnicity_test.exs
@@ -5,9 +5,25 @@ defmodule Epicenter.Cases.Import.EthnicityTest do
 
   describe "build_attrs" do
     test "creates map of parent and children ethnicity values from row values" do
-      %{"foo" => "bar", "ethnicity" => "Cuban"}
+      %{"foo" => "bar", "ethnicity" => ""}
       |> Ethnicity.build_attrs()
-      |> assert_eq(%{"foo" => "bar", "ethnicity" => %{"parent" => "Cuban", "children" => []}}, :simple)
+      |> assert_eq(%{"foo" => "bar", "ethnicity" => %{"parent" => nil, "children" => nil}}, :simple)
+
+      %{"foo" => "bar", "ethnicity" => "Unknown"}
+      |> Ethnicity.build_attrs()
+      |> assert_eq(%{"foo" => "bar", "ethnicity" => %{"parent" => nil, "children" => nil}}, :simple)
+
+      %{"foo" => "bar", "ethnicity" => "RefusedToAnswer"}
+      |> Ethnicity.build_attrs()
+      |> assert_eq(%{"foo" => "bar", "ethnicity" => %{"parent" => "Declined to answer", "children" => []}}, :simple)
+
+      %{"foo" => "bar", "ethnicity" => "NonHispanicOrNonLatino"}
+      |> Ethnicity.build_attrs()
+      |> assert_eq(%{"foo" => "bar", "ethnicity" => %{"parent" => "Not Hispanic, Latino/a, or Spanish origin", "children" => []}}, :simple)
+
+      %{"foo" => "bar", "ethnicity" => "HispanicOrLatino"}
+      |> Ethnicity.build_attrs()
+      |> assert_eq(%{"foo" => "bar", "ethnicity" => %{"parent" => "Hispanic, Latino/a, or Spanish origin", "children" => []}}, :simple)
     end
 
     test "does nothing crazy if there is no ethnicity value" do
