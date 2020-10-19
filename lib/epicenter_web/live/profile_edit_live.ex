3a373c71d035d2c80d88c5f175813b5bcc1a47ac
Sara Tansey <sara@geometer.io>
2020-10-19 14:41:09 -0400

    Change breadcrumbs on profile edit to back button
    [#175023057]
    
    Signed-off-by: Erik Hanson <erik@geometer.io>

--- lib/epicenter_web/live/profile_edit_live.ex
+++ lib/epicenter_web/live/profile_edit_live.ex
@@ -1,7 +1,7 @@
 defmodule EpicenterWeb.ProfileEditLive do
   use EpicenterWeb, :live_view
 
-  import EpicenterWeb.IconView, only: [plus_icon: 0, arrow_down_icon: 0, arrow_right_icon: 2, trash_icon: 0]
+  import EpicenterWeb.IconView, only: [plus_icon: 0, arrow_down_icon: 0, back_icon: 0, trash_icon: 0]
   import EpicenterWeb.LiveHelpers, only: [assign_defaults: 2, assign_page_title: 2, noreply: 1, ok: 1]
 
   alias Epicenter.AuditLog
