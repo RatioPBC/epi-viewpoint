f508548ffcb42b626d83d20bd298e90ee6156327
Mike Stallard <mike@geometer.io>
2020-10-21 09:55:24 -0700

    feat: hide admin menu for non-admins
    [#175257690]

--- test/epicenter_web/features/admin_test.exs
+++ test/epicenter_web/features/admin_test.exs
@@ -9,6 +9,20 @@ defmodule EpicenterWeb.Features.AdminTest do
 
   setup :register_and_log_in_user
 
+  test "admins can see the admin nav", %{conn: conn, user: user} do
+    user |> Accounts.update_user(%{admin: true}, Test.Fixtures.audit_meta(@admin))
+
+    conn
+    |> get("/people")
+    |> Pages.Navigation.assert_has_menu_item("Admin")
+  end
+
+  test "non-admins do not see the admin nav", %{conn: conn} do
+    conn
+    |> get("/people")
+    |> Pages.Navigation.refute_has_menu_item("Admin")
+  end
+
   test "admin can administer users", %{conn: conn, user: user} do
     user |> Accounts.update_user(%{admin: true}, Test.Fixtures.audit_meta(@admin))
 
---
+++ test/support/pages/navigation.ex
@@ -0,0 +1,20 @@
+defmodule EpicenterWeb.Test.Pages.Navigation do
+  import ExUnit.Assertions
+
+  alias Epicenter.Test
+  alias EpicenterWeb.Test.Pages
+
+  def assert_has_menu_item(view_or_conn_or_html, menu_item) do
+    assert menu_item in menu_items(view_or_conn_or_html)
+  end
+
+  def refute_has_menu_item(view_or_conn_or_html, menu_item) do
+    refute menu_item in menu_items(view_or_conn_or_html)
+  end
+
+  defp menu_items(view_or_conn_or_html),
+    do:
+      view_or_conn_or_html
+      |> Pages.parse()
+      |> Test.Html.all("#user-menu li", as: :text)
+end
