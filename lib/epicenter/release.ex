85a7e65fd892f5219f5cbb64693a735076cd518f
Harold Tafur <harold@geometer.io>
2020-10-27 09:48:19 -0700

    move seeds back to seeds because yagni in prod
    Signed-off-by: Sara Tansey <sara@geometer.io>

--- lib/epicenter/release.ex
+++ lib/epicenter/release.ex
@@ -23,37 +23,6 @@ defmodule Epicenter.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
-  def seeds do
-    IO.puts("RUNNING SEEDS...")
-
-    existing_user_tids = Epicenter.Accounts.list_users() |> Euclid.Extra.Enum.pluck(:tid)
-
-    new_users =
-      [
-        {"superuser", "Sal Superuser", true},
-        {"admin", "Amy Admin", true},
-        {"investigator", "Ida Investigator", false},
-        {"tracer", "Tom Tracer", false}
-      ]
-      |> Enum.reject(fn {tid, _name, _admin?} -> tid in existing_user_tids end)
-
-    for {tid, name, admin?} <- new_users do
-      email = "#{tid}@example.com"
-      password = "password123"
-
-      IO.puts("Creating #{name} / #{email} / #{password}")
-
-      Epicenter.Accounts.register_user!({
-        %{email: email, password: password, tid: tid, name: name, admin: admin?},
-        %Epicenter.AuditLog.Meta{
-          author_id: Application.get_env(:epicenter, :unpersisted_admin_id),
-          reason_action: AuditLog.Revision.register_user_action(),
-          reason_event: AuditLog.Revision.seed_event()
-        }
-      })
-    end
-  end
-
   defp repos do
     Application.load(@app)
     Application.fetch_env!(@app, :ecto_repos)
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,5 +11,32 @@
 # and so on) as they will fail if something goes wrong.
 
 if Application.get_env(:epicenter, :seeds_enabled?) do
-  Epicenter.Release.seeds()
+  IO.puts("RUNNING SEEDS...")
+
+  existing_user_tids = Epicenter.Accounts.list_users() |> Euclid.Extra.Enum.pluck(:tid)
+
+  new_users =
+    [
+      {"superuser", "Sal Superuser", true},
+      {"admin", "Amy Admin", true},
+      {"investigator", "Ida Investigator", false},
+      {"tracer", "Tom Tracer", false}
+    ]
+    |> Enum.reject(fn {tid, _name, _admin?} -> tid in existing_user_tids end)
+
+  for {tid, name, admin?} <- new_users do
+    email = "#{tid}@example.com"
+    password = "password123"
+
+    IO.puts("Creating #{name} / #{email} / #{password}")
+
+    Epicenter.Accounts.register_user!({
+      %{email: email, password: password, tid: tid, name: name, admin: admin?},
+      %Epicenter.AuditLog.Meta{
+        author_id: Application.get_env(:epicenter, :unpersisted_admin_id),
+        reason_action: Epicenter.AuditLog.Revision.register_user_action(),
+        reason_event: Epicenter.AuditLog.Revision.seed_event()
+      }
+    })
+  end
 end
