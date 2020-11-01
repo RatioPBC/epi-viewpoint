e593128459cc68326243f88ad7c20b88a63125a4
Byron Anderson <byron@geometer.io>
2020-11-01 15:25:53 -0700

    migration to make users table 'disabled' flag not null

---
+++ priv/repo/migrations/20201101221029_make_users_disabled_field_not_null.exs
@@ -0,0 +1,10 @@
+defmodule Epicenter.Repo.Migrations.MakeUsersDisabledFieldNotNull do
+  use Ecto.Migration
+
+  def change do
+    execute("update users set disabled=false where disabled is null;", "")
+    alter table(:users) do
+      modify :disabled, :boolean, default: false, null: false, from: :boolean
+    end
+  end
+end
