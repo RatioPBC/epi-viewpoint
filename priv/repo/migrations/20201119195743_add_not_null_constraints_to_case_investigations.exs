ca62f696177c804e524f45c4370627065af27954
Mike Stallard <mike@geometer.io>
2020-11-19 12:05:43 -0800

    feat: ensure that person_id and initiating_lab_result_id are not null
    [#175381844]
    
    Signed-off-by: Harold Tafur <harold@geometer.io>

---
+++ priv/repo/migrations/20201119195743_add_not_null_constraints_to_case_investigations.exs
@@ -0,0 +1,17 @@
+defmodule Epicenter.Repo.Migrations.AddNotNullConstraintsToCaseInvestigations do
+  use Ecto.Migration
+
+  def up do
+    alter table(:case_investigations) do
+      modify :person_id, :binary_id, null: false
+      modify :initiating_lab_result_id, :binary_id, null: false
+    end
+  end
+
+  def down do
+    alter table(:case_investigations) do
+      modify :person_id, :binary_id, null: true
+      modify :initiating_lab_result_id, :binary_id, null: true
+    end
+  end
+end
