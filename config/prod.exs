35fde6f413eb5cb5c8d6b051614ceb0eeb4facaa
Pat Maddox <pat@geometer.io>
2020-11-19 18:13:37 -0800

    Disable SSL. The load balancer handles it now.

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,6 @@ use Mix.Config
 # before starting your production server.
 config :epicenter, EpicenterWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true, rewrite_on: [:x_forwarded_proto]],
   server: true,
   url: [host: {:system, "CANONICAL_HOST"}, port: 443, scheme: "https"]
 
@@ -30,40 +29,3 @@ config :logger_json, :backend,
 config :epicenter, Epicenter.Repo,
   adapter: Ecto.Adapters.Postgres,
   log: false
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :epicenter, EpicenterWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-#         transport_options: [socket_opts: [:inet6]]
-#       ]
-#
-# The `cipher_suite` is set to `:strong` to support only the
-# latest and more secure SSL ciphers. This means old browsers
-# and clients may not be supported. You can set it to
-# `:compatible` for wider support.
-#
-# `:keyfile` and `:certfile` expect an absolute path to the key
-# and cert in disk or a relative path inside priv, for example
-# "priv/ssl/server.key". For all supported SSL configuration
-# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-#
-# We also recommend setting `force_ssl` in your endpoint, ensuring
-# no data is ever sent via http, always redirecting to https:
-#
-#     config :epicenter, EpicenterWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
