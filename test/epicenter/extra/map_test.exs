a69af30f19b881e09b4fd774174d68bb63dc5488
Erik Hanson <erik@geometer.io>
2020-11-30 07:22:11 -0800

    Add `get_in`, `delete_in`, and `put_in` functions to Extra.Map, which act like the similarly-named functions in `Access` but have some features better suited to working with nested maps.
    [#175768801]

--- test/epicenter/extra/map_test.exs
+++ test/epicenter/extra/map_test.exs
@@ -3,6 +3,56 @@ defmodule Epicenter.Extra.MapTest do
 
   alias Epicenter.Extra
 
+  describe "delete_in" do
+    test "removes the value at the keypath" do
+      %{a: %{b: %{c: 1, d: 2}, e: 3}} |> Extra.Map.delete_in([:a, :b, :c]) |> assert_eq(%{a: %{b: %{d: 2}, e: 3}}, :simple)
+    end
+
+    test "does nothing if there is nothing at the keypath" do
+      %{a: %{b: 1}} |> Extra.Map.delete_in([:a, :b, :c]) |> assert_eq(%{a: %{b: 1}}, :simple)
+    end
+  end
+
+  describe "get_in" do
+    test "gets the value at the keypath" do
+      %{a: %{b: [1, 2]}} |> Extra.Map.get_in([:a, :b]) |> assert_eq([1, 2], :simple)
+      %{"a" => %{"b" => [1, 2]}} |> Extra.Map.get_in(["a", "b"]) |> assert_eq([1, 2], :simple)
+    end
+
+    test "returns nil if there is nothing at the keypath" do
+      %{a: %{b: [1, 2]}} |> Extra.Map.get_in([:a, :c]) |> assert_eq(nil)
+      %{a: %{b: 1}} |> Extra.Map.get_in([:a, :b, :c]) |> assert_eq(nil)
+    end
+  end
+
+  describe "put_in" do
+    test "with :replace, if the new value is already in the map, does nothing" do
+      %{a: %{b: 1}} |> Extra.Map.put_in([:a, :b], 1, on_conflict: :replace) |> assert_eq(%{a: %{b: 1}}, :simple)
+    end
+
+    test "with :replace, if the keypath doesn't exist, the new value is added at the keypath" do
+      %{} |> Extra.Map.put_in([:x, :y], 10, on_conflict: :replace) |> assert_eq(%{x: %{y: 10}}, :simple)
+      %{a: %{b: 1}} |> Extra.Map.put_in([:x, :y], 10, on_conflict: :replace) |> assert_eq(%{a: %{b: 1}, x: %{y: 10}}, :simple)
+    end
+
+    test "with :replace, if a value exists at the keypath, it is replaced" do
+      %{a: %{b: 1}} |> Extra.Map.put_in([:a, :b], 10, on_conflict: :replace) |> assert_eq(%{a: %{b: 10}}, :simple)
+      %{a: %{b: [1, 2]}} |> Extra.Map.put_in([:a, :b], 10, on_conflict: :replace) |> assert_eq(%{a: %{b: 10}}, :simple)
+    end
+
+    test "with :list_append, if the existing value contains the new value, nothing is changed" do
+      %{a: %{b: [1, 2]}} |> Extra.Map.put_in([:a, :b], 1, on_conflict: :list_append) |> assert_eq(%{a: %{b: [1, 2]}}, :simple)
+    end
+
+    test "with :list_append, if the keypath exists, its value is replaced with a list containing the old value and the new value" do
+      %{a: %{b: 1}} |> Extra.Map.put_in([:a, :b], 10, on_conflict: :list_append) |> assert_eq(%{a: %{b: [1, 10]}}, :simple)
+    end
+
+    test "with :list_append, if the keypath exists and its value is a list, the new value is appended" do
+      %{a: %{b: [1, 2]}} |> Extra.Map.put_in([:a, :b], 10, on_conflict: :list_append) |> assert_eq(%{a: %{b: [1, 2, 10]}}, :simple)
+    end
+  end
+
   describe "to_list :depth_first" do
     test "converts a map of 'key -> value' or 'key -> values' into a list by alphabetizing keys and traversing depth-first" do
       %{"z" => "z1", "a" => "a1"} |> Extra.Map.to_list(:depth_first) |> assert_eq(["a", "a1", "z", "z1"])
