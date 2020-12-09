6cc676245d0677b9a8230b134d992842153c81bd
Harold Tafur <harold@tafurs.com>
2020-12-09 08:34:08 -0800

    remove IO.inspect

--- lib/epicenter_web/views/form.ex
+++ lib/epicenter_web/views/form.ex
@@ -135,9 +135,9 @@ defmodule EpicenterWeb.Form do
   @doc "opts: span, sticky"
   def save_button(%Form.Line{} = line, opts \\ []) do
     data_opts = grid_data(1, line, opts) |> Keyword.put(:role, "save-button")
+
     submit("Save", data: data_opts)
     |> add_to_line(line, opts)
-    # |> IO.inspect()
   end
 
   @doc "opts: span"
