1bd9be9e7eb964a80bf678dd95a87dd01a6bd929
Pat Maddox <pat@geometer.io>
2020-12-10 12:17:53 -0800

    test: add initial render test of note component
    [#175694270]
    
    Signed-off-by: Mike Stallard <mike@geometer.io>

---
+++ test/epicenter_web/live_components/investigation_note_component_test.exs
@@ -0,0 +1,132 @@
+defmodule EpicenterWeb.InvestigationNoteComponentTest do
+  use EpicenterWeb.ConnCase, async: true
+
+  import EpicenterWeb.LiveComponent.Helpers
+  import Phoenix.LiveViewTest
+
+  alias Epicenter.Test
+  alias EpicenterWeb.InvestigationNoteComponent
+
+  defmodule TestLiveView do
+    use EpicenterWeb, :live_view
+
+    import EpicenterWeb.LiveComponent.Helpers
+    import EpicenterWeb.LiveHelpers, only: [noreply: 1]
+
+    alias Epicenter.Accounts.User
+    alias Epicenter.Cases.InvestigationNote
+    alias EpicenterWeb.InvestigationNoteComponent
+
+    def mount(_params, _session, socket) do
+      note = %InvestigationNote{
+        id: "test-note-id",
+        text: "Hello, this is a note",
+        author_id: "test-author-id",
+        author: %User{id: "test-author-id", name: "Alice Testuser"},
+        inserted_at: ~U[2020-10-31 10:30:00Z]
+      }
+
+      {:ok, socket |> assign(note: note, on_delete: &Function.identity/1)}
+    end
+
+    def render(assigns) do
+      ~H"""
+      = component(@socket,
+            InvestigationNoteComponent,
+            "renders-a-note",
+            note: @note,
+            current_user_id: "test-user",
+            on_delete: @on_delete)
+      """
+    end
+
+    def handle_info({:assigns, new_assigns}, socket) do
+      socket |> assign(new_assigns) |> noreply()
+    end
+  end
+
+  describe "initial render" do
+    test "renders a note", %{conn: conn} do
+      {:ok, view, _html} = live_isolated(conn, TestLiveView)
+
+      [note_details] =
+        view
+        |> render()
+        |> Test.Html.parse()
+        |> Test.Html.all("[data-role=investigation-note]", fn note_el ->
+          id = Test.Html.attr(note_el, "data-note-id") |> List.first()
+          text = Test.Html.find(note_el, "[data-role=investigation-note-text]") |> Test.Html.text()
+          author = Test.Html.find(note_el, "[data-role=investigation-note-author]") |> Test.Html.text()
+          date = Test.Html.find(note_el, "[data-role=investigation-note-date]") |> Test.Html.text()
+          %{id: id, text: text, author: author, date: date}
+        end)
+
+      # TODO: something like this?
+      #        |> Test.Html.find("[data-role=investigation-note]")
+      #        |> note_attributes()
+
+      assert %{
+               id: "test-note-id",
+               text: "Hello, this is a note",
+               author: "Alice Testuser",
+               date: "10/31/2020"
+             } = note_details
+    end
+  end
+
+  #  describe "typing text into the text area" do
+  #    test "shows the save button", %{conn: conn} do
+  #      {:ok, view, _html} = live_isolated(conn, TestLiveView)
+  #
+  #      view |> element("form") |> render_change(%{"form_field_data" => %{"text" => "A new note"}})
+  #
+  #      assert has_element?(view, "button[data-role='save-button']")
+  #    end
+  #  end
+  #
+  #  describe "submitting the form" do
+  #    test "calls the on_add callback, passing the note attrs", %{conn: conn} do
+  #      pid = self()
+  #      on_add = fn note_attrs -> send(pid, {:received_on_add, note_attrs}) end
+  #      {:ok, view, _html} = live_isolated(conn, TestLiveView)
+  #
+  #      send(view.pid, {:assigns, on_add: on_add})
+  #
+  #      view |> element("form") |> render_submit(%{"form_field_data" => %{"text" => "A new note"}})
+  #
+  #      assert_receive {:received_on_add, %{author_id: "test-user", text: "A new note"}}
+  #    end
+  #
+  #    test "clears the form", %{conn: conn} do
+  #      {:ok, view, _html} = live_isolated(conn, TestLiveView)
+  #
+  #      view |> element("form") |> render_change(%{"form_field_data" => %{"text" => "A new note"}})
+  #      %{"form_field_data[text]" => text} = Pages.form_state(view)
+  #      assert text |> Euclid.Exists.present?()
+  #
+  #      view |> element("form") |> render_submit(%{"form_field_data" => %{"text" => "A new note"}})
+  #      %{"form_field_data[text]" => text} = Pages.form_state(view)
+  #      assert text |> Euclid.Exists.blank?()
+  #    end
+  #  end
+  #
+  #  describe "validation" do
+  #    test "shows an error when there is no text", %{conn: conn} do
+  #      {:ok, view, _html} = live_isolated(conn, TestLiveView)
+  #      view |> element("form") |> render_submit(%{"form_field_data" => %{"text" => ""}})
+  #
+  #      assert has_element?(view, ".invalid-feedback[phx-feedback-for='form_field_data_text']")
+  #    end
+  #
+  #    test "does not call on_add when there is no text", %{conn: conn} do
+  #      pid = self()
+  #      on_add = fn note_attrs -> send(pid, {:received_on_add, note_attrs}) end
+  #
+  #      {:ok, view, _html} = live_isolated(conn, TestLiveView)
+  #      send(view.pid, {:assigns, on_add: on_add})
+  #      view |> element("form") |> render_submit(%{"form_field_data" => %{"text" => ""}})
+  #
+  #      refute_receive {:received_on_add, _}
+  #    end
+  #  end
+end
