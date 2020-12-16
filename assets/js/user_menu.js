8a17bb57d280ea1314aeec5c1534b7c5a2d19972
Erik Hanson <erik@geometer.io>
2020-12-16 07:12:49 -0800

    Fix issue with user menu caused by putting its markup under liveview's control.
    One side-effect is that clicking on the menu title no longer hides the menu. We could probably fix that in the future.
    
    [#175935993]

--- assets/js/user_menu.js
+++ assets/js/user_menu.js
@@ -1,14 +1,15 @@
 export let UserMenu = {
   setup() {
-    document.querySelectorAll("#user-menu").forEach((userMenu) => {
+    document.querySelectorAll("#user-menu-title").forEach((userMenu) => {
       userMenu.addEventListener("click", (event) => {
         let target = document.querySelector("#user-menu-list");
-        target.style.display = target.style.display === "block" ? "none" : "block";
+        target.style.display = "block";
+        event.stopPropagation();
       });
     });
 
     document.addEventListener("click", (e) => {
-      document.querySelectorAll("#user-menu").forEach((userMenu) => {
+      document.querySelectorAll("#user-menu-list").forEach((userMenu) => {
         if (!userMenu.contains(e.target)) {
           document.querySelectorAll("[role=menu]").forEach((menu) => {
             if (menu.style.display === "block") {
