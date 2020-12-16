c5450b1e65354c72c75e883d88f3853e97a8d150
Mike Stallard <mike@geometer.io>
2020-12-16 15:55:00 -0800

    tooling: update demo url

--- bin/_support/deployments.sh
+++ bin/_support/deployments.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 export STAGING_BASE_URL=https://viewpoint.staging.gsi.dev
-export DEMO_BASE_URL=https://viewpoint.demo.geometer.dev
+export DEMO_BASE_URL=https://viewpoint.demo.gsi.dev
