1af76eb6f766e8a4388aa940f03d760c81f8d228
Erik Hanson <erik@geometer.io>
2020-12-17 12:21:26 -0800

    Add test for styleguide to keep it from breaking in the future :)

---
+++ test/epicenter_web/live/styleguide_live_test.exs
@@ -0,0 +1,33 @@
+defmodule EpicenterWeb.StyleguideLiveTest do
+  use EpicenterWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+
+  alias EpicenterWeb.Test.Pages
+
+  describe "renders without error" do
+    test "main styleguide page", %{conn: conn} do
+      conn |> Pages.visit("/styleguide") |> render()
+    end
+
+    @tag :skip
+    test "form styleguide page", %{conn: conn} do
+      conn |> Pages.visit("/styleguide/form") |> render()
+    end
+
+    @tag :skip
+    test "form-builder styleguide page", %{conn: conn} do
+      conn |> Pages.visit("/styleguide/form-builder") |> render()
+    end
+
+    @tag :skip
+    test "form-multiselect styleguide page", %{conn: conn} do
+      conn |> Pages.visit("/styleguide/form-multiselect") |> render()
+    end
+
+    @tag :skip
+    test "investigation notes styleguide page", %{conn: conn} do
+      conn |> Pages.visit("/styleguide/investigation-notes-section") |> render()
+    end
+  end
+end
