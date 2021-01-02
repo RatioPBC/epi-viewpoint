5c4a695042dc341e3963c92cba3d9fcd55640c87
Erik Hanson <erik@geometer.io>
2021-01-02 05:47:56 -0800

    Don't put parens around calls to assert_that

--- .formatter.exs
+++ .formatter.exs
@@ -2,5 +2,6 @@
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   line_length: 150,
+  locals_without_parens: [assert_that: :*],
   subdirectories: ["priv/*/migrations"]
 ]
