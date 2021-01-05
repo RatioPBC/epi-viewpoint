ea21e9e3d420f01d5340c0a4d15b9fcf6d244318
JB Steadman <jb@geometer.io>
2021-01-05 11:53:28 -0800

    Adding not-null constraint to user_agent field on logins table
    Signed-off-by: Harold Tafur <harold@geometer.io>

---
+++ priv/repo/migrations/20210105193509_add_null_constraint_to_user_agent.exs
@@ -0,0 +1,15 @@
+defmodule Epicenter.Repo.Migrations.AddNullConstraintToUserAgent do
+  use Ecto.Migration
+
+  def up do
+    alter table(:logins) do
+      modify :user_agent, :text, null: false
+    end
+  end
+
+  def down do
+    alter table(:logins) do
+      modify :user_agent, :text, null: true
+    end
+  end
+end
