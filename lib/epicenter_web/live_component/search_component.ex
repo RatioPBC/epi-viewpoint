51ff2b0c330c5a6390ec30c7b4a4c6cd8f9434a5
Devin Brown <devin@geometer.io>
2021-01-13 17:10:55 -0600

    Searching for a person by external id takes you to their profile
    [#176223685]
    
    Signed-off-by: Sara Tansey <sara@geometer.io>

--- lib/epicenter_web/live_component/search_component.ex
+++ lib/epicenter_web/live_component/search_component.ex
@@ -2,4 +2,22 @@ defmodule EpicenterWeb.SearchComponent do
   use EpicenterWeb, :live_component
 
   import EpicenterWeb.IconView, only: [search_icon: 0]
+
+  import EpicenterWeb.LiveHelpers,
+    only: [noreply: 1]
+
+  alias Epicenter.Cases
+
+  def handle_event("search", %{"search_form" => %{"term" => term}}, socket) do
+    term = term |> String.trim()
+
+    socket =
+      case Cases.find_person_id_by_external_id(term) |> IO.inspect(label: "found person id") do
+        nil -> socket
+        person_id -> socket |> push_redirect(to: Routes.profile_path(socket, EpicenterWeb.ProfileLive, person_id))
+      end
+
+    socket
+    |> noreply()
+  end
 end
--- test/epicenter_web/features/search_test.exs
+++ test/epicenter_web/features/search_test.exs
@@ -3,6 +3,8 @@ defmodule EpicenterWeb.Features.SearchTest do
 
   import Phoenix.ConnTest
 
+  alias Epicenter.Test
+  alias Epicenter.Cases
   alias EpicenterWeb.Test.Pages
 
   setup :register_and_log_in_user
@@ -11,5 +13,37 @@ defmodule EpicenterWeb.Features.SearchTest do
     conn
     |> get("/people")
     |> Pages.Navigation.assert_has_search_field()
+  end
+
+  describe "searching by ODRS id" do
+    test "search for known person displays person page", %{conn: conn, user: user} do
+      external_id = "10004"
+      {:ok, person} = Test.Fixtures.person_attrs(user, "person") |> Cases.create_person()
+      {:ok, _} = Test.Fixtures.demographic_attrs(user, person, "first", %{external_id: external_id}) |> Cases.create_demographic()
+
+      conn
+      |> Pages.People.visit()
+      |> Pages.submit_and_follow_redirect(conn, "[data-role=app-search] form", %{search_form: %{"term" => external_id}})
+      |> Pages.Profile.assert_here(person)
+    end
+
+    test "searching for an existing person with extraneous whitespace", %{conn: conn, user: user} do
+      external_id = "10004"
+      whitespaced_external_id = "\t10004  "
+      {:ok, person} = Test.Fixtures.person_attrs(user, "person") |> Cases.create_person()
+
+      {:ok, _} =
+        Test.Fixtures.demographic_attrs(user, person, "first", %{external_id: external_id})
+        |> Cases.create_demographic()
+
+      conn
+      |> Pages.People.visit()
+      |> Pages.submit_and_follow_redirect(conn, "[data-role=app-search] form", %{search_form: %{"term" => whitespaced_external_id}})
+      |> Pages.Profile.assert_here(person)
+    end
+
+    # test "search for unknown person displays no results page", %{conn: conn, user: user} do
+
+    # end
   end
 end
