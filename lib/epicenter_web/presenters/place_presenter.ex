16530af52d4e37300eb392a25c40befd7749173e
Pat Maddox <pat@ratiopbc.com>
2021-12-16 16:48:53 -0800

    Fix: Render a Place's address as an empty string if it has no addresses

--- lib/epicenter_web/presenters/place_presenter.ex
+++ lib/epicenter_web/presenters/place_presenter.ex
@@ -2,6 +2,8 @@ defmodule EpicenterWeb.Presenters.PlacePresenter do
   alias Epicenter.Cases.Place
   alias EpicenterWeb.Format
 
+  def address(%Place{place_addresses: []}), do: ""
+
   def address(%Place{} = place) do
     Format.address(hd(place.place_addresses))
   end
